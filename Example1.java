8579f21bedd88b71038e56f1b3f08d1a987ef8f8
siamaksc <siamaksc@kth.se>
2016-10-04 12:56:33 +0200

    The first commit

---
+++ Example1.java
@@ -0,0 +1,10 @@
+package excersise019.ex01;
+
+public class Example1 {
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		  if (!weekday || vacation){
+		    return true;
+		  }
+		  return false;
+		}
+}
